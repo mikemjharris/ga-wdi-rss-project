fc0274c5289228590569b6f5821f8fa06c2d2e9e
mikemjharris <mike.mj.harris@gmail.com>
2014-05-17 11:37:07 +0100

    added delete feed button

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,4 +3,11 @@ class FeedsController < ApplicationController
 	def create
 	end
 
+  def destroy
+      Feed.find(params[:id]).articles.destroy
+      Feed.find(params[:id]).destroy
+      redirect_to :root
+
+  end
+
 end
