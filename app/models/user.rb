820438dc7bf0788343bbf9e7e0b103de850406e3
NirajVyas <nirajvyas28@hotmail.com>
2014-05-18 16:28:45 +0100

    facebook added but not wokring properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, omniauth_providers: [:twitter, :google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable, omniauth_providers: [:twitter, :google_oauth2, :facebook]
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :first_name, :last_name
   # attr_accessible :title, :body
@@ -47,6 +47,25 @@ devise :database_authenticatable, :registerable,
     end
     
   end
+
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+      user = User.where(:provider => auth.provider, :uid => auth.uid).first
+      if user
+        return user
+      else
+        registered_user = User.where(:email => auth.info.email).first
+        if registered_user
+          return registered_user
+        else
+          user = User.create(name:auth.extra.raw_info.name,
+                              provider:auth.provider,
+                              uid:auth.uid,
+                              email:auth.info.email,
+                              password:Devise.friendly_token[0,20],
+                            )
+        end   
+      end
+    end
 
 
   
