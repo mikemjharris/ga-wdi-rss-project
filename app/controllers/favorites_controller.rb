95b75f700be496e7ef4ca8e864a36ffa5b578d0e
samuel-hill <samuel_hill@me.com>
2014-05-19 17:05:34 +0100

    Uupdated button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,24 @@ class FavoritesController < ApplicationController
 
 	def create
 		user = current_user
-		article = Article.find(params[:id])
-		user.mark_as_favorite [ article ]
-		redirect_to :back
+		@article = Article.find(params[:id])
+		user.mark_as_favorite [ @article ]
+
+		respond_to do |format|
+    		format.js {render '/articles/favorite.js.erb'}
+    		format.html { redirect_to :back }
+
+   		end	
 	end
 
 	def destroy
 		user = current_user
-		article = Article.find(params[:id])
-		user.remove_mark :favorite, [ article ]
-		redirect_to :back
+		@article = Article.find(params[:id])
+		user.remove_mark :favorite, [ @article ]
+		respond_to do |format|
+    		format.js {render '/articles/favorite.js.erb'}
+    		format.html { redirect_to :back }
+   		end	
 	end
 
 end
