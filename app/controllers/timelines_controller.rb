b67818a8b15ed5e84e90c97023e1148e65652f41
samuel-hill <samuel_hill@me.com>
2014-05-19 21:17:56 +0100

    AJAX on add to timeline button

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,16 +2,27 @@ class TimelinesController < ApplicationController
 
 	def create
 		user = current_user
-		article = Article.find(params[:id])
-		user.mark_as_timeline [ article ]
-		redirect_to :back
+		@article = Article.find(params[:id])
+		user.mark_as_timeline [ @article ]
+
+		respond_to do |format|
+    		format.js {render '/articles/timeline.js.erb'}
+    		format.html { redirect_to :back }
+
+   		end	
+		
 	end
 
 	def destroy
 		user = current_user
-		article = Article.find(params[:id])
-		user.remove_mark :timeline, [ article ]
-		redirect_to :back
+		@article = Article.find(params[:id])
+		user.remove_mark :timeline, [ @article ]
+		
+		respond_to do |format|
+    		format.js {render '/articles/timeline.js.erb'}
+    		format.html { redirect_to :back }
+
+   		end	
 	end
 
 end
