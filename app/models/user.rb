771353720002195539212df0250c4dbd3c7113bb
NirajVyas <nirajvyas28@hotmail.com>
2014-05-20 13:01:33 +0100

    ransack search added to articles page

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, omniauth_providers: [:twitter, :google_oauth2, :facebook]
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :first_name, :last_name, :profile_image
   # attr_accessible :title, :body
 
     def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
@@ -27,7 +27,7 @@ devise :database_authenticatable, :registerable,
                             provider:auth.provider,
                             uid:auth.uid,
                             email:auth.info.nickname.downcase + "@twitter.com",
-                            :profile_image => auth.image,
+                            profile_image: auth.info.image,
                             password:Devise.friendly_token[0,20],
                           )
       end
