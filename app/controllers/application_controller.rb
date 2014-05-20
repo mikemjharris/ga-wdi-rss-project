ece253990b323dff8f92fc4f0970693bb4521f00
mikemjharris <mike.mj.harris@gmail.com>
2014-05-20 21:12:33 +0100

    css tweaks to landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
 	include PublicActivity::StoreController
   protect_from_forgery
-  
+    
+  layout :layout
+
+  private
+  def layout
+    # only turn it off for login pages:
+    is_a?(Devise::SessionsController) ? "login" : "application"
+    # or turn layout off for every devise controller:
+    #devise_controller? && "application"
+  end
+
+
 end
