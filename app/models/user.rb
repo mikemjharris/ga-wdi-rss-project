047998bf340487ddb6b4f37a3f7ff892ddbd54de
mikemjharris <mike.mj.harris@gmail.com>
2014-05-20 11:01:57 +0100

    user info changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, omniauth_providers: [:twitter, :google_oauth2, :facebook]
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :first_name, :last_name, :profile_image
   # attr_accessible :title, :body
 
     def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
@@ -17,17 +17,17 @@ devise :database_authenticatable, :registerable,
     if user
       return user
     else
-      registered_user = User.where(:email => auth.uid + "@twitter.com").first
+      registered_user = User.where(:email => auth.info.nickname.downcase + "@twitter.com").first
       if registered_user
         return registered_user
 
       else
-
+        
         user = User.create(
                             provider:auth.provider,
                             uid:auth.uid,
-                            email:auth.info.nickname.downcase + "@twitter.com",
-                            :profile_image => auth.image,
+                            email: auth.info.nickname.downcase + "@twitter.com",
+                            profile_image: auth.image,
                             password:Devise.friendly_token[0,20],
                           )
       end
