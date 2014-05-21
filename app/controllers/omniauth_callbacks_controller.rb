c68ba137e3c4998b32bee7efad35e8b20e8cd785
mikemjharris <mike.mj.harris@gmail.com>
2014-05-21 17:04:55 +0100

    refactoring loging methods

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,19 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController   
 
-  def twitter
-    auth = env["omniauth.auth"]
 
-    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"],current_user)
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session["devise.twitter_uid"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url    
-    end
-  end
-
-   def google_oauth2
+  def google_oauth2
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
       flash.notice = "Signed in Through Google!"
@@ -25,16 +13,30 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  def facebook     
-     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)      
-     if @user.persisted?       
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+  def twitter
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      flash.notice = "Signed in Through Twitter!"
+      sign_in_and_redirect user
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      session["devise.user_attributes"] = user.attributes
+      flash.notice = "Problem creating account"
       redirect_to new_user_registration_url
     end
   end
+
+  def facebook
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      flash.notice = "Signed in Through Facebook!"
+      sign_in_and_redirect user
+    else
+      session["devise.user_attributes"] = user.attributes
+      flash.notice = "Problem creating account"
+      redirect_to new_user_registration_url
+    end
+  end
+
   
 
 
